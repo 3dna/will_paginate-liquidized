83ad42634d72a5c21c0c2951f44e2f20b3ee81ce
David Huie <david@nationbuilder.com>
2012-11-16 15:22:11 -0800

    Added methods necessary for nbuild

--- lib/will_paginate/liquidized/view_helpers.rb
+++ lib/will_paginate/liquidized/view_helpers.rb
@@ -1,50 +1,71 @@
-module WillPaginate::Liquidized   
+module WillPaginate::Liquidized
   module ViewHelpers
     include WillPaginate::ViewHelpers
-    
-    def will_paginate_liquid(collection, anchor = nil, prev_label = nil, next_label = nil)      
+
+    def will_paginate_liquid(collection, anchor = nil, prev_label = nil, next_label = nil)
       opts = {}
       opts[:previous_label] = prev_label if prev_label
-      opts[:next_label]     = next_label if next_label      
+      opts[:next_label]     = next_label if next_label
       opts[:params]         = {:anchor => anchor} if anchor
       opts[:controller]     = @context.registers[:controller]
-      
-      with_renderer 'WillPaginate::Liquidized::LinkRenderer' do 
+
+      with_renderer 'WillPaginate::Liquidized::LinkRenderer' do
         will_paginate *[collection, opts].compact
       end
     end
-    
+
+    alias_method :paginate, :will_paginate_liquid
+
     def with_renderer(renderer)
       old_renderer, options[:renderer] = options[:renderer], renderer
       result = yield
       options[:renderer] = old_renderer
       result
     end
-    
+
     def options
       WillPaginate::ViewHelpers.pagination_options
+    end
+
+    def page_entries_info(collection, options = {})
+      entry_name = options[:model] ||
+        (collection.empty?? 'entry' : collection.first.class.name.underscore.sub('_', ' '))
+
+      if collection.total_pages < 2
+        case collection.size
+        when 0; "No #{entry_name.pluralize} found"
+        when 1; "<b>1</b> #{entry_name}"
+        else;   "<b>All #{collection.size}</b> #{entry_name.pluralize}"
+        end
+      else
+        %{<b>%d&nbsp;-&nbsp;%d</b> of <b>%d</b> #{entry_name.pluralize}} % [
+                                                                            collection.offset + 1,
+                                                                            collection.offset + collection.length,
+                                                                            collection.total_entries
+                                                                           ]
+      end.html_safe
     end
   end
 
   class LinkRenderer < WillPaginate::LinkRenderer
-    
+
     include ActionView::Helpers::UrlHelper
-    include ActionView::Helpers::TagHelper 
+    include ActionView::Helpers::TagHelper
 
     def to_html
       return "<p><strong style=\"color:red;\">(Will Paginate Liquidized) Error:</strong> you must pass a controller in Liquid render call; <br/>
               e.g. Liquid::Template.parse(\"{{ movies | will_paginate_liquid }}\").render({'movies' => @movies}, :registers => {:controller => @controller})</p>" unless @options[:controller]
-      
+
       links = @options[:page_links] ? windowed_links : []
       # previous/next buttons added in to the links collection
       links.unshift page_link_or_span(@collection.previous_page, 'disabled prev_page', @options[:previous_label])
       links.push    page_link_or_span(@collection.next_page,     'disabled next_page', @options[:next_label])
-      
+
       html = links.join(@options[:separator])
       html_attributes.delete(:controller)
       @options[:container] ? content_tag(:div, html, html_attributes) : html
     end
-    
+
     def page_link(page, text, attributes = {})
       link_to text, url_for_page(page), attributes
     end
@@ -52,7 +73,7 @@ module WillPaginate::Liquidized
     def page_span(page, text, attributes = {})
       content_tag :span, text, attributes
     end
-              
+
     def url_for_page(page)
       page_one = page == 1
       unless @url_string and !page_one
@@ -61,11 +82,11 @@ module WillPaginate::Liquidized
         # page links should preserve GET parameters
         stringified_merge @url_params, @controller.params if @controller && @controller.request.get? && @controller.params
         stringified_merge @url_params, @options[:params] if @options[:params]
-        
+
         if complex = param_name.index(/[^\w-]/)
           page_param = (defined?(CGIMethods) ? CGIMethods : ActionController::AbstractRequest).
             parse_query_parameters("#{param_name}=#{page}")
-          
+
           stringified_merge @url_params, page_param
         else
           @url_params[param_name] = page_one ? 1 : 2
@@ -74,7 +95,7 @@ module WillPaginate::Liquidized
         url = @controller.url_for(@url_params)
         url = "#{url}##{@options[:params][:anchor]}" if @options[:params] && @options[:params][:anchor]
         return url if page_one
-        
+
         if complex
           @url_string = url.sub(%r!((?:\?|&amp;)#{CGI.escape param_name}=)#{page}!, '\1@')
           return url
@@ -92,5 +113,5 @@ module WillPaginate::Liquidized
       # finally!
       @url_string.sub '@', page.to_s
     end
-  end  
-end        
+  end
+end
